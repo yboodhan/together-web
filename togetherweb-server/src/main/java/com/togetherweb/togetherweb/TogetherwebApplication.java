e4d63e3e9cd0d67163a39c2f40a84f449a7d285d
yboodhan <yboodhan@gmail.com>
2020-02-18 15:27:52 -0800

    Created server with default dependencies

---
+++ togetherweb-server/src/main/java/com/togetherweb/togetherweb/TogetherwebApplication.java
@@ -0,0 +1,13 @@
+package com.togetherweb.togetherweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogetherwebApplication.class, args);
+    }
+
+}
---
+++ togetherweb-server/src/test/java/com/togetherweb/togetherweb/TogetherwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togetherweb.togetherweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
