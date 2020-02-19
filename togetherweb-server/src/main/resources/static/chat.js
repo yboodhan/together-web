a57ce00c2952d581033ce752a0d9aef4556a62b6
yboodhan <yboodhan@gmail.com>
2020-02-19 09:55:12 -0800

    Added basic initial settings to chat.js

--- togetherweb-server/src/main/resources/static/chat.js
+++ togetherweb-server/src/main/resources/static/chat.js
@@ -3,5 +3,21 @@
 $(function() {
     'use strict';
 
+    // Initially, the client is null
+    var client = null;
+
+    // Initially, fields are set to disabled/unconnected
+    $('#connect,#disconnect,#text').prop('disabled', true);
+
+    // Initially, the message field is blank
+    $('#text').val("");
+
+    // On form submission, prevent page refresh
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+
     
+});
+
 }
